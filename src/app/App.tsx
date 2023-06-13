740c5e8b225c8748166e8b081017e53ee69c5e8d
mnyef <vladgav004@gmail.com>
2023-06-13 20:50:28 +0300

    AppRouter, Nav Bar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
-import { Route, Routes, Link } from 'react-router-dom';
-import { Suspense } from 'react';
+import { Link } from 'react-router-dom';
+
 import './styles/index.scss';
-import { classNames } from 'shared/config/lib/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/provider/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { AppRouter } from './provider/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -12,14 +12,8 @@ const App = () => {
   return (
     <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/'}>Main</Link>
-      <Link to={'/about'}>About</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage />} />
-          <Route path={'/'} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <Navbar />
+      <AppRouter />
     </div>
   );
 };
---
+++ src/widgets/Navbar/UI/Navbar.tsx
@@ -0,0 +1,27 @@
+classNames;
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Navbar.module.scss';
+import { AppLinkTheme, AppLink } from 'shared/UI/AppLink/AppLink';
+
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+  return (
+    <div className={classNames(cls.navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={'/'}
+          className={cls.mainLink}
+        >
+          Main
+        </AppLink>
+        <AppLink theme={AppLinkTheme.RED} to={'/about'}>
+          About
+        </AppLink>
+      </div>
+    </div>
+  );
+};
