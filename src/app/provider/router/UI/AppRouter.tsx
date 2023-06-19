98049740889b7bd905f61004f8e1d161361d8cb0
mnyef <vladgav004@gmail.com>
2023-06-19 13:48:37 +0300

    Add React testing library, Add tests for btn, sidebar

--- src/app/provider/router/UI/AppRouter.tsx
+++ src/app/provider/router/UI/AppRouter.tsx
@@ -4,19 +4,19 @@ import { PageLoader } from 'shared/UI/PageLoader/PageLoader';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 const AppRouter = () => (
-  <Routes>
-    {Object.values(routeConfig).map(({ element, path }) => (
-      <Route
-        key={path}
-        path={path}
-        element={
-          <Suspense fallback={<PageLoader />}>
-            <div className='page-wrapper'>{element}</div>
-          </Suspense>
-        }
-      />
-    ))}
-  </Routes>
+    <Routes>
+        {Object.values(routeConfig).map(({ element, path }) => (
+            <Route
+                key={path}
+                path={path}
+                element={(
+                    <Suspense fallback={<PageLoader />}>
+                        <div className="page-wrapper">{element}</div>
+                    </Suspense>
+                )}
+            />
+        ))}
+    </Routes>
 );
 
 export default AppRouter;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/provider/ThemeProvider';
+import { ErrorBoundary } from 'app/provider/ErrorBoundary';
 import App from './app/App';
 import 'shared/config/i18n/i18n';
-import { ErrorBoundary } from 'app/provider/ErrorBoundary';
 
 render(
-  <BrowserRouter>
-    <ErrorBoundary>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </ErrorBoundary>
-  </BrowserRouter>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
+    </BrowserRouter>,
+    document.getElementById('root'),
 );
