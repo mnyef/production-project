96f60327345fc387a756f37a28242b669a487a5f
mnyef <vladgav004@gmail.com>
2023-06-22 16:18:39 +0300

    Fix linter

--- src/widgets/Sidebar/UI/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/UI/Sidebar/Sidebar.tsx
@@ -10,26 +10,26 @@ interface SidebarProps {
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
-    const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
 
-    const onToggle = () => {
-        setCollapsed((prev) => !prev);
-    };
+  const onToggle = () => {
+    setCollapsed((prev) => !prev);
+  };
 
-    return (
-        <div
-            data-testid="sidebar"
-            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
-                className,
-            ])}
-        >
-            <Button data-testid="sidebar-toggle" onClick={onToggle}>
+  return (
+    <div
+      data-testid='sidebar'
+      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
+        className,
+      ])}
+    >
+      {/* <Button data-testid="sidebar-toggle" onClick={onToggle}>
                 toggle
-            </Button>
-            <div className={cls.switchers}>
-                <ThemeSwitcher />
-                <LangSwitcher className={cls.lang} />
-            </div>
-        </div>
-    );
+            </Button> */}
+      <div className={cls.switchers}>
+        <ThemeSwitcher />
+        <LangSwitcher className={cls.lang} />
+      </div>
+    </div>
+  );
 };
