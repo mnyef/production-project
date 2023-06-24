24b2e1b3c4fb834f79acf4efa7800f1bfa2cb784
mnyef <vladgav004@gmail.com>
2023-06-24 19:03:56 +0300

    Add Redux and first entity

---
+++ src/app/provider/StoreProvider/index.ts
@@ -0,0 +1,5 @@
+import { StoreProvider } from './UI/StoreProvider';
+import { createReduxStore } from './config/store';
+import type { StateSchema } from './config/StateSchema';
+
+export { StoreProvider, createReduxStore, StateSchema };
