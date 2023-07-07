bc40322291d763a495ceb0fb493649d02c2bfa64
mnyef <vladgav004@gmail.com>
2023-07-07 14:05:06 +0300

    Add articles page

---
+++ src/pages/ArticleDetailsPage/index.ts
@@ -0,0 +1,1 @@
+export { ArticleDetailsPageAsync as ArticleDetailsPage } from './UI/ArticleDetailsPage/ArticleDetailsPage.async';
---
+++ src/pages/ArticlesPage/index.ts
@@ -0,0 +1,1 @@
+export { ArticlesPageAsync as ArticlesPage } from './UI/ArticlesPage/ArticlesPage.async';
